127621df77c5ff62739a3e7ccf5914d556e38c73
Aman Ahamed <75929022+Aman235-code@users.noreply.github.com>
2024-06-03 21:44:46 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:5000";
-export const IF = "http://localhost:5000/images/";
+export const URL = "https://code-tales-blog-be.onrender.com";
+export const IF = "https://code-tales-blog-be.onrender.com/images/";
